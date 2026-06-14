7393003bb04fadf0b5e5315d7ad1f8a46e70e718
SagwaProductions <ogigorffvv@gmail.com>
2026-06-14 18:28:26 +0200

    Update script.js
    Eh fixes

--- script.js
+++ script.js
@@ -7,3 +7,14 @@ const firebaseConfig = {
     messagingSenderId: "474638621860",
     appId: "1:474638621860:web:c55eea29fc4f30b740da31"
 };
+
+firebase.initializeApp(firebaseConfig);
+
+// Pobieranie danych z bazy
+const db = firebase.database();
+const playerRef = db.ref('Players/Player1/Swag'); // Sprawdź czy ścieżka się zgadza!
+
+playerRef.on('value', (snapshot) => {
+    const val = snapshot.val();
+    document.getElementById('coins').innerText = val !== null ? val : 0;
+});
